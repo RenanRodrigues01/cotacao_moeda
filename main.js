c5d2c36f3cb904e55c776b89b8f34d174c974467
renan <renan.rodriguesmoura2022@gmail.com>
2022-11-03 23:14:23 -0300

    refatorando main js

--- main.js
+++ main.js
@@ -3,43 +3,33 @@ const moedas = 'USD-BRL,EUR-BRL';
 const botaoDolar = document.querySelector('.botaoDolar');
 const botaoEuro = document.querySelector('.botaoEuro');
 
-botaoDolar.addEventListener('click',converteDolar);
 
-botaoEuro.addEventListener('click', converteEuro);
-
-function converteDolar (){
+botaoDolar.addEventListener('click', (evento) => {
     fetch(url + moedas)
-        .then(function(response){
-            return response.json()
-        })
-        .then(function(data){
-            const dolar = data.USDBRL;
-            
+        .then(response => response.json())
+        .then(data => {
+            const dolar = data.USDBRL
             document.getElementById('title').innerHTML = dolar.name;
-            document.getElementById('valorAtual').innerHTML = 
-            parseFloat(dolar.bid).toLocaleString('pr-br',{
-                style: 'currency',
-                currency: 'BRL'
+            document.getElementById('valorAtual').innerHTML=parseFloat(dolar.bid).toLocaleString('pr-br',{
+                    style: 'currency',
+                    currency: 'BRL'
             })
-            
+           
         })
-}
+});
 
-function converteEuro (){
+botaoEuro.addEventListener('click', (evento) => {
     fetch(url + moedas)
-        .then(function(response){
-            return response.json()
-        })
-        .then(function(data){
-            const euro = data.EURBRL;
-            
+        .then(response => response.json())
+        .then(data => {
+            const euro = data.EURBRL
             document.getElementById('title').innerHTML = euro.name;
             document.getElementById('valorAtual').innerHTML = 
             parseFloat(euro.bid).toLocaleString('pr-br',{
                 style: 'currency',
                 currency: 'BRL'
             })
-            
         })
-}
+});
+
 
