0fb83ede0a2f6dbc0609c2e4a361692001b22172
renan <renan.rodriguesmoura2022@gmail.com>
2022-11-04 23:16:39 -0300

    adicionando estilos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,22 @@
 const url = ' https://economia.awesomeapi.com.br/last/';
 const moedas = 'USD-BRL,EUR-BRL';
-const botaoDolar = document.querySelector('.botaoDolar');
-const botaoEuro = document.querySelector('.botaoEuro');
+const botaoDolar = document.querySelector('#botaoDolar');
+const botaoEuro = document.querySelector('#botaoEuro');
+const botaoConverter = document.querySelector('.converter')
 
 
 botaoDolar.addEventListener('click', (evento) => {
     fetch(url + moedas)
-        .then(response => response.json())
-        .then(data => {
-            const dolar = data.USDBRL
-            document.getElementById('title').innerHTML = dolar.name;
-            document.getElementById('valorAtual').innerHTML=parseFloat(dolar.bid).toLocaleString('pr-br',{
-                    style: 'currency',
-                    currency: 'BRL'
+        .then(response =>{ response.json()
+            .then(data => {
+                const dolar = data.USDBRL
+                document.getElementById('title').innerHTML = dolar.name;
+                document.getElementById('valorAtual').innerHTML=parseFloat(dolar.bid).toLocaleString('pr-br',{
+                        style: 'currency',
+                        currency: 'BRL'
+                })
+                
             })
-           
         })
 });
 
@@ -31,5 +33,3 @@ botaoEuro.addEventListener('click', (evento) => {
             })
         })
 });
-
-
